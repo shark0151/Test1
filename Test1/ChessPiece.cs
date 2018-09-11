5650592fb51e061e3d8bb51763a4af255ecaaecf
Vlad F <vlad.faget@outlook.com>
2018-09-12 00:31:55 +0300

    New way of defining chessboard and chess pieces

--- Test1/ChessPiece.cs
+++ Test1/ChessPiece.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace Test1
 {
     public class ChessPiece
     {
-        public ChessPiece()
-        {
-            
-        }
-
         private bool TeamColour;
         private string PieceType;
+        private bool HasPiece = false;
+        private Image PieceImage = new Image();
 
+        public ChessPiece()
+        {
+            PieceImage.Width = 80;
+            PieceImage.Height = 80;
+        }
+        
         public bool GetPieceTeam()
         {
             //false = black, true = white
@@ -37,6 +42,19 @@ namespace Test1
             TeamColour = team;
         }
 
-                
+        public void SetPieceActive(bool pieceactive)
+        {
+            HasPiece = pieceactive;
+        }
+        public Image GetImage()
+        {
+            return PieceImage;
+        }
+        public void SetImage(BitmapImage imgsource)
+        {
+            PieceImage.Source = imgsource;
+        }
+
+
     }
 }
--- Test1/Game.xaml.cs
+++ Test1/Game.xaml.cs
@@ -25,13 +25,13 @@ namespace Test1
     public sealed partial class Game : Page
     {
         private List<string> PieceTypes = new List<string>(new string[] { "Pawn", "Knight", "Bishop", "Rook", "Queen", "King" }); //0-p,1-k,2-b,3-r,4-q,5-k
-        private List<ChessPieceUnit> BlackPieces = new List<ChessPieceUnit>();
+        private List<ChessboardSquare> BlackPieces = new List<ChessboardSquare>();
         private List<List<ChessboardSquare>> BoardArray = new List<List<ChessboardSquare>>();
         public Game()
         {
             this.InitializeComponent();
-            SetTable();
             InitTable();
+            SetTable();            
 
             void InitTable()
             {
@@ -44,7 +44,6 @@ namespace Test1
                         x.SetLocation(new Thickness(80 * i, 80 * j, 0, 0), "" + i + j);
                         BoardArray[i].Add(x);
                         Chessboard.Children.Add(BoardArray[i][j].GetSquare());
-                        //Chessboard.Children.Add(x.GetSquare());
                     }
                 }
             }
@@ -54,78 +53,61 @@ namespace Test1
                 {
                     for (int j = 0; j < 2; j++)
                     {
-                        ChessPieceUnit x = new ChessPieceUnit();
+                        
                         if (j == 0)
                         {
-                            x.SetPieceTeam(false);
-                            Image PieceImage = new Image();
-                            if (i == 0 || i == 7) { x.SetPieceType(PieceTypes[3]); PieceImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/pieces/b_rook.png")); }
-                            else if (i == 1 || i == 6) { x.SetPieceType(PieceTypes[2]); PieceImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/pieces/b_knight.png")); }
-                            else if (i == 2 || i == 5) { x.SetPieceType(PieceTypes[1]); PieceImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/pieces/b_bishop.png")); }
-                            else if (i == 3) { x.SetPieceType(PieceTypes[4]); PieceImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/pieces/b_queen.png")); }
-                            else if (i == 4) { x.SetPieceType(PieceTypes[4]); PieceImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/pieces/b_king.png")); }
-                            PieceImage.Width = 80;
-                            PieceImage.Height = 80;
-                            PieceImage.HorizontalAlignment = HorizontalAlignment.Left;
-                            PieceImage.VerticalAlignment = VerticalAlignment.Top;
-                            PieceImage.Margin = new Thickness(80 * i, 0, 0, 0);
-                            Chessboard.Children.Add(PieceImage);
-
+                            BoardArray[i][j].SetPieceTeam(false);                            
+                            if (i == 0 || i == 7) { BoardArray[i][j].SetPieceType(PieceTypes[3]); BoardArray[i][j].SetImage(new BitmapImage(new Uri("ms-appx:///Assets/pieces/b_rook.png"))); BoardArray[i][j].SetPieceActive(true); }
+                            else if (i == 1 || i == 6) { BoardArray[i][j].SetPieceType(PieceTypes[2]); BoardArray[i][j].SetImage(new BitmapImage(new Uri("ms-appx:///Assets/pieces/b_knight.png"))); BoardArray[i][j].SetPieceActive(true); }
+                            else if (i == 2 || i == 5) { BoardArray[i][j].SetPieceType(PieceTypes[1]); BoardArray[i][j].SetImage(new BitmapImage(new Uri("ms-appx:///Assets/pieces/b_bishop.png"))); BoardArray[i][j].SetPieceActive(true); }
+                            else if (i == 3) { BoardArray[i][j].SetPieceType(PieceTypes[4]); BoardArray[i][j].SetImage(new BitmapImage(new Uri("ms-appx:///Assets/pieces/b_queen.png"))); BoardArray[i][j].SetPieceActive(true); }
+                            else if (i == 4) { BoardArray[i][j].SetPieceType(PieceTypes[4]); BoardArray[i][j].SetImage(new BitmapImage(new Uri("ms-appx:///Assets/pieces/b_king.png"))); BoardArray[i][j].SetPieceActive(true); }
+                           
                         }
                         else if (j == 1)
                         {
-                            x.SetPieceType(PieceTypes[0]);
-                            x.SetPieceTeam(false);
-                            Image PieceImage = new Image();
-                            PieceImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/pieces/b_pawn.png"));
-                            PieceImage.Width = 80;
-                            PieceImage.Height = 80;
-                            PieceImage.HorizontalAlignment = HorizontalAlignment.Left;
-                            PieceImage.VerticalAlignment = VerticalAlignment.Top;
-                            PieceImage.Margin = new Thickness(80 * i, 80, 0, 0);
-                            Chessboard.Children.Add(PieceImage);
-
+                            BoardArray[i][j].SetPieceType(PieceTypes[0]);
+                            BoardArray[i][j].SetPieceTeam(false);
+                            BoardArray[i][j].SetImage(new BitmapImage(new Uri("ms-appx:///Assets/pieces/b_pawn.png")));
+                            BoardArray[i][j].SetPieceActive(true);
                         }
 
-                        BlackPieces.Add(x);
+                        BlackPieces.Add(BoardArray[i][j]);
                     }
                 }
             }
             
         }
 
-        public class ChessPieceUnit : ChessPiece
+        public class ChessboardSquare : ChessPiece
         {
-            public ChessPieceUnit()
-            {
-                Image PieceImage = new Image();
-                PieceImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/pieces/b_bishop.png"));
-                PieceImage.Width = 80;
-                PieceImage.Height = 80;
-                
-            }
-        }
-
-        public class ChessboardSquare
-        {
-            private Button EmptySquare = new Button();
+            private Grid ChessSquare = new Grid();
+            private Button SquareSelect = new Button();
+            
             public ChessboardSquare()
             {                
-                EmptySquare.Width = 80;
-                EmptySquare.Height = 80;
-                EmptySquare.HorizontalAlignment = HorizontalAlignment.Left;
-                EmptySquare.VerticalAlignment = VerticalAlignment.Top;
-                EmptySquare.Margin = new Thickness(0, 0, 0, 0);     //I need to store i,j somewhere so i can access the squares later and a way to add pieces to them
-                EmptySquare.Content = "gg";
+                SquareSelect.Width = 80;
+                SquareSelect.Height = 80;
+                SquareSelect.HorizontalAlignment = HorizontalAlignment.Center;
+                SquareSelect.VerticalAlignment = VerticalAlignment.Center;
+                SquareSelect.Margin = new Thickness(0, 0, 0, 0);
+                SquareSelect.Content = "";
+
+                ChessSquare.Width = 80;
+                ChessSquare.HorizontalAlignment = HorizontalAlignment.Left;
+                ChessSquare.VerticalAlignment = VerticalAlignment.Top;
+                ChessSquare.Height = 80;
+                ChessSquare.Children.Add(GetImage());
+                ChessSquare.Children.Add(SquareSelect);
             }
             public void SetLocation(Thickness x, string y)
             {
-                EmptySquare.Margin = x;
-                EmptySquare.Content = y;
+                ChessSquare.Margin = x;
+                SquareSelect.Content = y;
             }
-            public Button GetSquare()
+            public Grid GetSquare()
             {
-                return EmptySquare;
+                return ChessSquare;
             }
         }
 
